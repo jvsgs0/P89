647e2842d580195e03b431aa716a6e9f7e757612
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-12-04 19:07:24 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbx-qaMzf3xYf3piSQtw_d1uyTI_H2i-U",
+  authDomain: "narracao-de-historia-1a028.firebaseapp.com",
+  databaseURL: "https://narracao-de-historia-1a028-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "narracao-de-historia-1a028",
+  storageBucket: "narracao-de-historia-1a028.appspot.com",
+  messagingSenderId: "257115197348",
+  appId: "1:257115197348:web:eeffcd187333f5814c7b31"
+};
